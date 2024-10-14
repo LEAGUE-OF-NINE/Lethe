fecb57e167f2603a964ccc5b9aea485301eacba8
pufferffish <me@windtfw.com>
2024-10-14 19:58:23 +0100

    WHAT THE FUCK

--- src/Patches/Skin.cs
+++ src/Patches/Skin.cs
@@ -1,8 +1,10 @@
+using System;
 using Addressable;
 using HarmonyLib;
 using Il2CppSystem.IO;
 using SD;
 using System.Linq;
+using Il2CppSystem.Collections.Generic;
 using UnhollowerRuntimeLib;
 using UnityEngine;
 
@@ -14,20 +16,99 @@ public class Skin : MonoBehaviour
     public static void Setup(Harmony harmony)
     {
         ClassInjector.RegisterTypeInIl2Cpp<Skin>();
-        foreach (var declaredMethod in AccessTools.GetDeclaredMethods(typeof(SDCharacterSkinUtil)))
-            if (declaredMethod.Name == nameof(SDCharacterSkinUtil.CreateSkin))
-            {
-                CustomEncounterHook.LOG.LogInfo($"Patching {declaredMethod.Name}");
-                var isDirect = declaredMethod.GetParameters().ToArray().Any(x => x.Name == "appearanceID");
-                var hookName = isDirect ? nameof(CreateSkin) : nameof(CreateSkinForModel);
-                harmony.Patch(declaredMethod, new HarmonyMethod(AccessTools.Method(typeof(Skin), hookName)));
-            }
+        var original = typeof(AddressableManager)
+            .GetMethod(nameof(AddressableManager.LoadAssetSync))?
+            .MakeGenericMethod(typeof(UnityEngine.GameObject));
+        if (original == null)
+        {
+            throw new Exception("Failed to find AddressableManager.LoadAssetSync");
+        }
+        
+        harmony.Patch(
+            original,
+            prefix: new HarmonyMethod(typeof(Skin).GetMethod(nameof(PatchLoadAssetSync)))
+        );
     }
     
-    public static bool CreateSkinForModel(BattleUnitView view, BattleUnitModel unit, Transform parent,
-        ref CharacterAppearance __result)
+    public static void What() {}
+    
+    public static bool PatchLoadAssetSync(ref string label, ref string resourceId, Transform parent, ref Il2CppSystem.ValueTuple<UnityEngine.GameObject, DelegateEvent> __result)
     {
-        return CreateSkin(view, unit.GetAppearanceID(), parent, ref __result);
+        CustomEncounterHook.LOG.LogInfo($"Loading {label}: {resourceId}");
+        
+        var prefix = "!abno_";
+        if (resourceId.StartsWith(prefix))
+        {
+            label = SDCharacterSkinUtil._LABEL_ABNORMALITY;
+            resourceId = resourceId.Substring(prefix.Length);
+            CustomEncounterHook.LOG.LogInfo($"- Patched to {label}: {resourceId}");
+            return true;
+        }
+
+        prefix = "!enemy_";
+        if (resourceId.StartsWith(prefix))
+        {
+            label = SDCharacterSkinUtil._LABEL_ENEMY;
+            resourceId = resourceId.Substring(prefix.Length);
+            CustomEncounterHook.LOG.LogInfo($"- Patched to {label}: {resourceId}");
+            return true;
+        }
+
+        prefix = "!identity_";
+        if (resourceId.StartsWith(prefix))
+        {
+            label = SDCharacterSkinUtil._LABEL_PERSONALITY;
+            resourceId = resourceId.Substring(prefix.Length);
+            CustomEncounterHook.LOG.LogInfo($"- Patched to {label}: {resourceId}");
+            return true;
+        }
+
+        prefix = "!custom_";
+        if (resourceId.StartsWith(prefix))
+        {
+            var appearanceID = resourceId.Substring(prefix.Length);
+            var prefabPath = "";
+            if (appearanceID.Contains("/"))
+            {
+                var appearancePath = appearanceID.Split("/".ToCharArray(), 2);
+                appearanceID = appearancePath[0];
+                prefabPath = appearancePath[1];
+            }
+
+            var path = Path.Combine(CustomEncounterHook.CustomAppearanceDir.FullName, appearanceID + ".bundle");
+            if (!File.Exists(path))
+            {
+                CustomEncounterHook.LOG.LogError("Cannot find asset bundle at: " + path);
+                return true;
+            }
+
+            var bundle = AssetBundle.LoadFromFile(path);
+            try
+            {
+                if (prefabPath == "")
+                {
+                    foreach (var assetName in bundle.AllAssetNames()) CustomEncounterHook.LOG.LogInfo("Found asset: " + assetName);
+
+                    prefabPath = bundle.AllAssetNames()[0];
+                }
+
+                CustomEncounterHook.LOG.LogInfo("Using prefab: " + prefabPath);
+                var asset = bundle.LoadAsset(prefabPath);
+                var gameObject = Instantiate(asset, parent.position, parent.rotation, parent).Cast<GameObject>();
+                __result = new();
+                __result.Item1 = gameObject;
+                __result.Item2 = (Action) What;
+
+                CustomEncounterHook.LOG.LogError(appearanceID + ": appearance is null!?");
+                return false;
+            }
+            finally
+            {
+                bundle.Unload(false);
+            }
+        }
+
+        return true;
     }
 
     public static bool CreateSkin(BattleUnitView view, string appearanceID, Transform parent,
