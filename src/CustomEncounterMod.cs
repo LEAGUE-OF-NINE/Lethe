7b0714f6459819dc2c03e055e37dbe4b18219f11
rook <m9.pacheco.co@gmail.com>
2024-10-28 16:40:45 -0700

    yes

--- src/CustomEncounterMod.cs
+++ src/CustomEncounterMod.cs
@@ -85,7 +85,7 @@ public class CustomEncounterMod : BasePlugin
             Passives.RollAllHeads.Setup(harmony);
             Passives.ChangeAppearance.Setup(harmony);
             if (toggleCRC.Value) Patches.TextAsset.Setup(harmony);
-            SuperClash.Setup(harmony);
+            Patches.Skills.Setup(harmony);
 
             EncounterHelper.Log = Log;
             if (!File.Exists(EncounterConfig)) File.Create(EncounterConfig).Close();
