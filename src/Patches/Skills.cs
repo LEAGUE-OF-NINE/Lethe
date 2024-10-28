e317c13df9e115719a0ccc44ba43ea03ebf8857a
pufferffish <me@windtfw.com>
2024-10-28 23:27:53 +0000

    configurable duel view customizer

--- src/Patches/Skills.cs
+++ src/Patches/Skills.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using CustomEncounter.SkillAbility;
 using HarmonyLib;
@@ -19,7 +20,18 @@ public class Skills : Il2CppSystem.Object
     [HarmonyPrefix]
     private static void PatchGetType(ref string typeName)
     {
-        if (typeName.StartsWith("BattleDuelViewer_")) typeName = "BattleDuelViewer_8389";
+        if (!typeName.StartsWith("BattleDuelViewer_")) return;
+        if (!int.TryParse(typeName.Substring("BattleDuelViewer_".Length), out var viewerID)) return;
+        var skillData = Singleton<StaticDataManager>.Instance.SkillList.GetData(viewerID);
+        if (skillData == null) return;
+        foreach (var abilityData in skillData.GetAbilityScript(4))
+        {
+            var scriptName = abilityData.scriptName;
+            if (!scriptName.Contains("DuelViewer_")) continue;
+            var appearanceId = scriptName.Replace("DuelViewer_", "");
+            typeName = $"BattleDuelViewer_{appearanceId}";
+            return;
+        }
     }
 
 }
