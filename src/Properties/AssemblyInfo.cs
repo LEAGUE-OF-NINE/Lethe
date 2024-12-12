13e7d93c612a6a8ecb1acbb2f8ee8c757f71f8bb
rook <m9.pacheco.co@gmail.com>
2024-12-12 14:38:32 -0800

    idk

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,13 +5,7 @@ using BaseMod;
 [assembly: AssemblyTitle(main.NAME)]
 [assembly: AssemblyCompany(main.AUTHOR)]
 [assembly: AssemblyProduct(main.NAME)]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
-
-
+[assembly: AssemblyCopyright("Copyright ©  skibiditoilet")]
 [assembly: ComVisible(false)]
-
-
-[assembly: Guid("b8f92b29-0e76-4151-b668-3b545ab0de25")]
-
 [assembly: AssemblyVersion(main.VERSION)]
 [assembly: AssemblyFileVersion(main.VERSION)]
--- src/main.cs
+++ src/main.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Diagnostics;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.IL2CPP;
-using HarmonyLib;
-using Il2CppDumper;
-using Il2CppSystem.IO;
-using Il2CppSystem.Security.Cryptography;
-using UnhollowerBaseLib;
-using UnhollowerRuntimeLib;
+using BepInEx.Logging;
+using System.IO;
 using UnityEngine;
-using Random = System.Random;
 
 namespace BaseMod;
 
@@ -18,12 +11,57 @@ namespace BaseMod;
 public class main : BasePlugin
 {
     public const string GUID = AUTHOR + "." + NAME;
-    public const string NAME = "TestPlugin";
-    public const string VERSION = "0.0.1";
-    public const string AUTHOR = "Noop";
+    public const string NAME = "lethe_experimental_v2";
+    public const string VERSION = "1.0.0";
+    public const string AUTHOR = "what";
+
+    //bepinex stuff
+    public static new ManualLogSource Log;
+    public static ConfigFile Configuration;
+
+    public static DirectoryInfo pluginPath = Directory.CreateDirectory(Path.Combine(Paths.PluginPath, $"{NAME}"));
+    public static DirectoryInfo vanillaDumpPath = Directory.CreateDirectory(Path.Combine(pluginPath.FullName, "dumpedData"));
+    public static DirectoryInfo templatePath = Directory.CreateDirectory(Path.Combine(pluginPath.FullName, "ModTemplate"));
+    public static DirectoryInfo modsPath = Directory.CreateDirectory(Path.Combine(pluginPath.FullName, "mods"));
 
     public override void Load()
     {
-        
+        Log = base.Log;
+    }
+
+    public static void LoadConfiguration()
+    {
+        //crc toggle config
+        Configuration.Bind
+        (
+            "LaunchSettings",
+            "ToggleCRC",
+            false,
+            "Toggle file checks from the game. Makes the game load faster. Disable when downloading updates."
+        );
+
+        Configuration.Bind
+        (
+            "LaunchSettings",
+            "ServerURL",
+            "https://api.lethelc.site/",
+            "The URL of the private server (e.g. http://127.0.0.1:8080), leave blank to use defaults."
+        );
+
+        Configuration.Bind
+        (
+           "LaunchSettings",
+           "usePrivateServer",
+           true,
+           "risky strats"
+        );
+
+        Configuration.Bind
+        (
+            "General",
+            "ReloadData",
+            KeyCode.Alpha8,
+            "Key bind to reload data ingame."
+        );
     }
 }
