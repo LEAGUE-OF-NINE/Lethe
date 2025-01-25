e71bc2ce95dacd778e76491f08183697eebc551b
R894 <54629307+R894@users.noreply.github.com>
2025-01-25 08:34:29 +0200

    add battlelog redundancy

--- src/Patches/BattleLog.cs
+++ src/Patches/BattleLog.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using HarmonyLib;
 using Server;
@@ -39,8 +40,36 @@ public class BattleLog : Il2CppSystem.Object
 
     [HarmonyPatch(typeof(UnityWebRequest), nameof(UnityWebRequest.Post), typeof(string), typeof(string))]
     [HarmonyPrefix]
-    public static bool PrePostRequest(string uri, string postData)
+    public static bool PrePostRequest(ref string uri, string postData)
     {
+        var serverURL = LetheMain.ConfigServer.Value;
+        if (!string.IsNullOrEmpty(serverURL))
+        {
+            try
+            {
+                var originalUri = new Uri(uri);
+                var newServerUri = new Uri(serverURL);
+                var newUri = new UriBuilder(originalUri)
+                {
+                    Host = newServerUri.Host,
+                    Scheme = newServerUri.Scheme,
+                    Port = newServerUri.IsDefaultPort ? -1 : newServerUri.Port
+                };
+
+                uri = newUri.ToString();
+            }
+            catch (UriFormatException ex)
+            {
+                LetheHooks.LOG.LogError($"Error parsing URIs: {ex.Message}");
+                return false;
+            }
+        }
+        else
+        {
+            LetheHooks.LOG.LogError("No server URL configured. Blocking request.");
+            return false;
+        }
+
         foreach (var blocked in BlockedDomains)
         {
             if (uri.Contains(blocked))
@@ -49,7 +78,7 @@ public class BattleLog : Il2CppSystem.Object
                 return false;
             }
         }
+
         return true;
     }
-
 }
